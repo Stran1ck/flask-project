24b26465b4e550cfcb00e8e441bd834f2aec08b8
Stran1ck <andrvlad07@gmail.com>
2020-09-18 18:16:50 +0500

    Second commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from app import app
-import view
 from app import db
+from posts.blueprint import posts
+import view
+
+
+app.register_blueprint(posts, url_prefix='/blog')
 
 
 if __name__ == '__main__':
---
+++ app/manage.py
@@ -0,0 +1,6 @@
+from app import maneger
+from main import *
+
+
+if __name__ == '__main__':
+    maneger.run()
